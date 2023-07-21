3fd6dcca66bb4c994c336c838789ecb656cf0e8f
hespnod <hespnod2348@gmail.com>
2023-07-21 21:29:49 +0530

    showing value of form on page and storing in local too

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 let form = document.getElementById('inp-form');
 form.addEventListener('submit', addform);
+let ul = document.getElementById('ul-list')
 
 function addform(e) {
     e.preventDefault();
     let myobj = {
         name: e.target[0].value,
-        email: e.target[1].value
+        email: e.target[1].value,
+        phone: e.target[2].value
     };
-    let myobj_serialised = JSON.stringify(myobj);
-    localStorage.setItem("myobj", myobj_serialised);
-
+    let myobj_string = JSON.stringify(myobj);
+    localStorage.setItem(`${myobj.email}`, myobj_string);
+    let textNode = document.createTextNode(`Name: ${myobj.name} Email: ${myobj.email} Phone: ${myobj.phone}`)
+    let li = document.createElement('li');
+    li.appendChild(textNode);
+    ul.appendChild(li);
+    e.target[0].value = ''
+    e.target[1].value = ''
+    e.target[2].value = ''
 }
