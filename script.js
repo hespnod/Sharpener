f2d81e52dba5746ee4db0ae1317928c468a5c000
hespnod <hespnod2348@gmail.com>
2023-07-21 19:46:41 +0530

    storing the data of use as object in localstorage

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ form.addEventListener('submit', addform);
 
 function addform(e) {
     e.preventDefault();
-    localStorage.setItem("name", e.target[0].value);
-    localStorage.setItem("email", e.target[1].value);
+    let myobj = {
+        name: e.target[0].value,
+        email: e.target[1].value
+    };
+    let myobj_serialised = JSON.stringify(myobj);
+    localStorage.setItem("myobj", myobj_serialised);
+
 }
