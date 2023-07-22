d7d59119da302c63524ce677af7f3dfb044bef81
hespnod <hespnod2348@gmail.com>
2023-07-22 08:49:22 +0530

    deleting the details from UI

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let form = document.getElementById('inp-form');
 form.addEventListener('submit', addform);
 let ul = document.getElementById('ul-list')
+ul.addEventListener('click', remove);
+
 
 function addform(e) {
     e.preventDefault();
@@ -14,8 +16,19 @@ function addform(e) {
     let textNode = document.createTextNode(`Name: ${myobj.name} Email: ${myobj.email} Phone: ${myobj.phone}`)
     let li = document.createElement('li');
     li.appendChild(textNode);
+    let btn = document.createElement('button');
+    btn.innerText = 'Delete';
+    btn.className = 'btn-del';
+    li.appendChild(btn);
     ul.appendChild(li);
+    console.log(btn.parentElement);
     e.target[0].value = ''
     e.target[1].value = ''
     e.target[2].value = ''
+}
+
+function remove(e) {
+    if (e.target.classList.contains('btn-del')) {
+        ul.removeChild(e.target.parentElement);
+    }
 }
